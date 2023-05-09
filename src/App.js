b60e92555ff1accc7f3c801cfb145bb3a6a4609b
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2023-05-09 10:34:21 -0400

    adding homepage to package.json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Timer from './Timer'
+import logo from "./logo.svg";
+import "./App.css";
+import Timer from "./Timer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        About
-      </header>
-      <section>
-        <Timer/>
-      </section>
+      <header className="App-header">About</header>
+
+      <Timer />
     </div>
   );
 }
