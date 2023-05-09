007bfbb7c8dccf1549420fa89ac0c9c7bed65b5e
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2023-05-09 10:18:56 -0400

    uploading build files

---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import "./Timer.css";
+
+function Timer() {
+  return (
+    <div className="Timer">
+      <div className="timer-text">
+        00:00
+      </div>
+    </div>
+  );
+}
+
+export default Timer;
