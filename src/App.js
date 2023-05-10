904c8f5757835557ba38b1dbfec5cfc1f5e13ec7
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2023-05-09 22:29:35 -0400

    setting up spotify connection and web playback sdk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from "./logo.svg";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Timer from "./Timer";
+import Login from "./Login";
+import WebPlayback from "./WebPlayback";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await fetch("/auth/token");
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">About</header>
-
       <Timer />
+
+      <>{token === "" ? <Login /> : <WebPlayback token={token} />}</>
     </div>
   );
 }
